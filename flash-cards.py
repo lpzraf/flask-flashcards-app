c5a86565687e8f3c4a974f67254053b2a7ee68be
Rafael Lopez <lpz.raf@gmail.com>
2020-08-09 15:51:54 -0400

    It will update the welcome template to incliude a for loop for the json eles

--- flash-cards.py
+++ flash-cards.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from model import db
+# abort is used to return a 404 page
 
 app = Flask(__name__)
 
-counter = 0
 @app.route('/')
 def welcome():
-    global counter
-    counter += 1
     return render_template(
         "welcome.html",
-        welcome="Bienvenido! You have visisted this page " + str(counter) + " times."
+        cards=db
         )
 
-@app.route("/card")
-def card_view():
-    card = db[0]
-    return render_template("card.html", card=card)
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", 
+                                card=card,
+                                index=index,
+                                max_index= len(db)-1)
+    except IndexError:
+        abort(404)
