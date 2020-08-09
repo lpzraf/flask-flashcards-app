8d1b66d9d32e8a2114e2f8d05d860e737d595a51
Rafael Lopez <lpz.raf@gmail.com>
2020-08-09 15:06:27 -0400

    It will add a new branch for the initial code

--- flash-cards.py
+++ flash-cards.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from model import db
+# abort is used to return a 404 page
 
 app = Flask(__name__)
 
@@ -13,7 +14,12 @@ def welcome():
         welcome="Bienvenido! You have visisted this page " + str(counter) + " times."
         )
 
-@app.route("/card")
-def card_view():
-    card = db[0]
-    return render_template("card.html", card=card)
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", 
+                                card=card,
+                                index=index)
+    except IndexError:
+        abort(404)
