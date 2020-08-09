7d064e6bef8d287d47edc516c8879e5995775acc
Rafael Lopez <lpz.raf@gmail.com>
2020-08-09 18:38:30 -0400

    It will add remove card function and template

--- flash-cards.py
+++ flash-cards.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, abort
-from model import db
+from flask import (Flask, render_template, abort, jsonify, 
+                    request, redirect, url_for)
+from model import db, save_db
 # abort is used to return a 404 page
 
 app = Flask(__name__)
@@ -21,3 +22,40 @@ def card_view(index):
                                 max_index= len(db)-1)
     except IndexError:
         abort(404)
+
+@app.route('/add_card', methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        # form has been submitted, process data
+        card = {"question": request.form['question'],
+                "answer": request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for('card_view', index=len(db)-1))
+    else:
+        # the user just requested the page, didnt submnit the form
+        return render_template("add_card.html")
+
+@app.route('/remove_card/<int:index>', methods=["GET", "POST"])
+def remove_card(index):
+    try:
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('welcome'))
+        else:
+            return render_template("remove_card.html", card=db[index])
+    except IndexError:
+        abort(404)
+
+@app.route('/api/card/')
+def api_card_list():
+    return jsonify(db)
+
+@app.route('/api/card/<int:index>')
+def api_card_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
+
